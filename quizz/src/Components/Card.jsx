733c6381c5f1269589f5f91bc40dc2279603d96c
Bodipoh <berylodipoh@gmail.com>
2022-10-07 02:51:10 +0300

    background image added

--- quizz/src/Components/Card.jsx
+++ quizz/src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => {
-  return <div className="frontcard">{props.children}</div>;
+  return <div className="frontcard" style={{ backgroundImage: 'url("/cube.png")' }}>{props.children} </div>;
 };
 
 export default Card;
